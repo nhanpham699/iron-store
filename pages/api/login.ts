87143a3b4464b4371b0674c245631f4d969b3570
nhanpham699 <phuocnhan699@gmail.com>
2021-12-17 16:06:19 +0700

    login page

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,9 +3,9 @@ import connectDB from "../../utils/mongodb"
 const bcrypt = require("bcrypt")
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  //   const salt = bcrypt.genSaltSync(10)
-  //   const hash = bcrypt.hashSync("thaitrinh220514", salt)
-  //   console.log(hash)
+  const salt = bcrypt.genSaltSync(10)
+  const hash = bcrypt.hashSync("thaitrinh220514", salt)
+  console.log(hash)
   res.status(200).json({ username: "Nhan dep trai" })
 }
 
