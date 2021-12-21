83704d17fa8239b0f84d02ad2faae02eb63fdcc2
nhanpham699 <phuocnhan699@gmail.com>
2021-12-21 15:42:35 +0700

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { withIronSessionSsr } from "iron-session/next"
 // import { InferGetServerSidePropsType } from "next"
 import React from "react"
-import Dashboard from "../components/Dashboard/Dashboard"
+import Dashboard from "../components/dashboard/Dashboard"
 import Producttable from "../components/Producttable"
 import { sessionOptions } from "../lib/iron-session"
 export const getServerSideProps = withIronSessionSsr(
